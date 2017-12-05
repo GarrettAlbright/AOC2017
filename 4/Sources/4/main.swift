0a22724f14cd1571ee96481153a2d12f967e5e39
Nocturnal <contact@nocturnal.solutions>
2017-12-05 10:24:22 -0700

    Day 4 part 2

--- 4/Sources/4/main.swift
+++ 4/Sources/4/main.swift
@@ -14,6 +14,7 @@ catch {
 }
 
 var valid: Int = 0
+var valid2: Int = 0
 
 for passphrase in input.split(separator: "\n") {
     let words = passphrase.split(separator: " ")
@@ -22,7 +23,14 @@ for passphrase in input.split(separator: "\n") {
     if words.count == Set(words).count {
         valid += 1
     }
+    // Abuse sets again to find anagrams since Set(["a", "b", "c"]) ==
+    // Set(["b", "c", "a"])
+    // Look at this mess oh my gosh this is write-only code
+    let wordsSet: Set<Set<String>> = Set(words.map({ word in Set(Array(word).map({ char in String(char) })) }))
+    if words.count == wordsSet.count {
+        valid2 += 1
+    }
 }
 
 print("Valid passphrases: \(valid)")
-
+print("Valid passphrases part 2: \(valid2)")
